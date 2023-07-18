bc0a6f225d52d0a2519d46a11817a0f924777d1a
Abd-al-qudus <Abd-al-qudus>
2023-07-18 09:32:11 +0000

    completed task 11 and 12

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -5,18 +5,19 @@
 
 if __name__ == "__main__":
     import pymongo
-    
+
     client = pymongo.MongoClient('mongodb://127.0.0.1:27017')
     db = client.logs
     collection = db.nginx
-    
+
     total_logs = collection.count_documents({})
     print("{} logs".format(total_logs))
-    
+
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         count = collection.count_documents({'method': method})
         print('method {}: {}'.format(method, count))
-        
-    status_check_count = collection.count_documents({'method': 'GET', 'path': '/status'})
+
+    status_check_count = collection.count_documents(
+        {'method': 'GET', 'path': '/status'})
     print('{} status check'.format(status_check_count))
