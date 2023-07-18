d69458d2aa3b905c6fe39b78088d429a17bd67d6
Abd-al-qudus <Abd-al-qudus>
2023-07-18 09:27:55 +0000

    completed task 11 and 12

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""return the collection count for the method
+    of nginx log file"""
+
+
+if __name__ == "__main__":
+    import pymongo
+    
+    client = pymongo.MongoClient()
+    db = client.logs
+    collection = db.nginx
+    
+    total_logs = collection.count_documents({})
+    print("{} logs".format(total_logs))
+    
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+    for method in methods:
+        count = collection.count_documents({'method': method})
+        print('method {}: {}'.format(method, count))
+        
+    status_check_count = collection.count_documents({'method': 'GET', 'path': '/status'})
+    print('{} status check'.format(status_check_count))
