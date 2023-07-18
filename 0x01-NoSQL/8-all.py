d3e07b1de5ca84a1e069b7a8411ba223c47752d9
Abd-al-qudus <Abd-al-qudus>
2023-07-18 08:37:21 +0000

    completed task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""return the elements in a mongo db collection"""
+
+def list_all(mongo_collection):
+    """return an empty list if collection is empty"""
+    return [documents for documents in mongo_collection.find()]
