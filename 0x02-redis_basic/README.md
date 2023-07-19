25ee11b0fcc19d17c01ea166919f888c294a935e
Abd-al-qudus <Abd-al-qudus>
2023-07-19 14:41:46 +0000

    remote dictionary service

---
+++ 0x02-redis_basic/README.md
@@ -0,0 +1,1 @@
+## Alx redis catching system
---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""a class that defines a redis instance"""
+import redis
+import uuid
+from typing import (
+    Any,
+    Callable,
+    Union
+)
+
+
+class Cache:
+    """create a redis instance and cache"""
+    def __init__(self) -> None:
+        self._redis: redis.Redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Any) -> str:
+        """takes and stores data in redis"""
+        key: str = str(uuid.uuid4())
+        self._redis.mset({key: data})
+        return key
+
+    def get(self, key: Union[str, bytes, int], fn: Callable = None) -> Any:
+        """get data from the redis storage"""
+        value = self._redis.get(key)
+        if value is None:
+            return None
+        if fn is not None:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        """get stored string from redis cache"""
+        return self.get(key, fn=lambda value: value.decode("utf-8"))
+
+    def get_int(self, key: int) -> int:
+        """get stored int from redis cache"""
+        return self.get(key, fn=int)
